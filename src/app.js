185a2163364fb32a2b2524d6b290db115b550d11
MarioTop <mariodelgt@gmail.com>
2020-05-28 16:55:57 -0600

    hghghg

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.set('port',process.env.PORT || 4000);
 
 app.set('views', path.join(__dirname, 'views'));
 
-app.engine('.hbs',exphbs({  
+/* app.engine('.hbs',exphbs({  
 
 defaultLayout:'main',
 layoutsDir: path.join(app.get('views'), 'layouts'),
@@ -26,7 +26,7 @@ partialsDir: path.join(app.get('views'), 'partials'),
 extname: '.hbs'
 
 }))
-app.set('view engine', 'hbs');
+app.set('view engine', 'hbs'); */
 
 
 
