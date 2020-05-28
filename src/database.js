84517a9d907f890d9b07e635ac0d16e9569eede2
MarioTop <mariodelgt@gmail.com>
2020-05-28 15:40:39 -0600

    fgfg

--- src/database.js
+++ src/database.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 /* console.log(process.env.MONGODB_URI);   */ 
 
- mongoose.connect(process.env.MONGODB_URI, {
+/*  mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true
 })
     .then(db => console.log('DB is connected to'))
-    .catch(err => console.error(err)); 
+    .catch(err => console.error(err));  */
